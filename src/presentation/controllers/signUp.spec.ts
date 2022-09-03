c932fb8cd1863caf3d2c7dd83d44d8854de85ec7
Yuri Witney <yuriwfbarros@gmail.com>
2022-09-03 12:11:59 -0300

    chore: test husky

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signUp'
 
-describe('SignUp  Controller', () => {
+describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
