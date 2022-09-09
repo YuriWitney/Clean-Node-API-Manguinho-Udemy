62ab595aa9ba8860600c1f498e25336fab5216c2
Yuri Witney <yuriwfbarros@gmail.com>
2022-09-08 21:10:12 -0300

    refactor: move all SignUpController protocols to a single file

--- src/presentation/controllers/SignUp/signUp.spec.ts
+++ src/presentation/controllers/SignUp/signUp.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './signUp'
-import { InvalidParamError, MissingParamError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols'
-import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
-import { AccountModel } from '../../domain/models/account'
+import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
+import { AddAccount, AddAccountModel, EmailValidator, AccountModel } from './signUpProtocols'
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
--- src/presentation/controllers/SignUp/signUp.ts
+++ src/presentation/controllers/SignUp/signUp.ts
@@ -1,7 +1,6 @@
-import { AddAccount } from '../../domain/usecases/add-account'
-import { InvalidParamError, MissingParamError } from '../errors'
-import { badRequest, serverError } from '../helpers/http-helper'
-import { HttpRequest, HttpResponse, EmailValidator, Controller } from '../protocols'
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { badRequest, serverError } from '../../helpers/http-helper'
+import { HttpRequest, HttpResponse, EmailValidator, Controller, AddAccount } from './signUpProtocols'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
