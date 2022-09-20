eeae72b6f7d0cbc97bd415193443819a0a625ba1
Barros, Yuri <2165225@cognizant.com>
2022-09-20 16:24:20 -0300

    feat: save last edits

--- src/data/usecases/addAccount/db-add-account.ts
+++ src/data/usecases/addAccount/db-add-account.ts
@@ -1,12 +1,12 @@
-import { AccountModel } from '../../../domain/models/account'
-import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
-import { Encrypter } from './protocols/encrypter'
+import { AddAccount, Encrypter, AddAccountModel, AccountModel, AddAccountRepository } from "./db-add-account-protocols"
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
